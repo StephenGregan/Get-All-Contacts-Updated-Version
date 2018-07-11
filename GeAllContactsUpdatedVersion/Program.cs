9ae50c5c1c2c4a6dad49d4eec2fef87c5b99ccda
StephenGregan <grego@live.ie>
2018-07-11 12:05:54 +0100

    initial commit

--- GeAllContactsUpdatedVersion/Program.cs
+++ GeAllContactsUpdatedVersion/Program.cs
@@ -45,7 +45,7 @@ namespace GetAllContactsFromInterpreterIntelligence
 
                 Console.WriteLine("Successfully logged in to ii.....\n");
 
-                var recordCount = 10400;
+                var recordCount = 11000;
                 var count = 0;
                 var recSkipped = false;
 
@@ -54,7 +54,7 @@ namespace GetAllContactsFromInterpreterIntelligence
                     try
                     {
 
-                        if (count < 11000)
+                        if (count < 1000)
                         {
                             if (recSkipped == true)
                             {
@@ -67,9 +67,74 @@ namespace GetAllContactsFromInterpreterIntelligence
                             var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/contact/" + count);
                             dynamic rootjson = JsonConvert.DeserializeObject(json);
                             //Console.WriteLine(json);
-                            File.AppendAllText(@"C:\Users\Ronan\source\repos\ContactLoader\Contacts\Schema_And_Data\stephenscontacts.json", JsonConvert.SerializeObject(rootjson));
-                            File.AppendAllText(@"C:\Users\Ronan\source\repos\ContactLoader\Contacts\Schema_And_Data\stephensContacts.json", Environment.NewLine);
-                            Console.WriteLine("Contents successfully writing to file.....Record: " + count + " Batch 1");
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts1.json", JsonConvert.SerializeObject(rootjson));
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts1.json", Environment.NewLine);
+                            Console.WriteLine("Contents successfully written to file.....Record: " + count + " Batch 1");
+                        }
+                        else if (count < 2000)
+                        {
+                            if (recSkipped == true)
+                            {
+                                recSkipped = false;
+                            }
+                            else
+                            {
+                                count++;
+                            }
+                            var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/contact/" + count);
+                            dynamic rootJson = JsonConvert.DeserializeObject(json);
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts2.json", JsonConvert.SerializeObject(rootJson));
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts2.json", Environment.NewLine);
+                            Console.WriteLine("Contents successfully written to file.....Record " + count + " batch 2");
+                        }
+                        else if (count < 3000)
+                        {
+                            if (recSkipped == true)
+                            {
+                                recSkipped = false;
+                            }
+                            else
+                            {
+                                count++;
+                            }
+                            var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/contact/" + count);
+                            dynamic rootJson = JsonConvert.DeserializeObject(json);
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts3.json", JsonConvert.SerializeObject(rootJson));
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts3.json", Environment.NewLine);
+                            Console.WriteLine("Contents successfully written to file.....Record " + count + " batch 3");
+                        }
+                        else if (count < 5000)
+                        {
+                            if (recSkipped == true)
+                            {
+                                recSkipped = false;
+                            }
+                            else
+                            {
+                                count++;
+                            }
+                            var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/contact/" + count);
+                            dynamic rootJson = JsonConvert.DeserializeObject(json);
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts4.json", JsonConvert.SerializeObject(rootJson));
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts4.json", Environment.NewLine);
+                            Console.WriteLine("Content successfully written to file.....Record " + count + " batch 4");
+                        }
+                        else
+                        {
+                            if (recSkipped == true)
+                            {
+                                recSkipped = false;
+                            }
+                            else
+                            {
+                                count++;
+                            }
+                            var json = client.DownloadString("https://tie.interpreterintelligence.com:443/api/contact/" + count);
+                            dynamic rootJson = JsonConvert.DeserializeObject(json);
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts5.json", JsonConvert.SerializeObject(rootJson));
+                            File.AppendAllText(@"C:\Users\Ronan\source\repos\GeAllContactsUpdatedVersion\GeAllContactsUpdatedVersion\allContacts5.json", Environment.NewLine);
+                            Console.WriteLine("Contents succesfully written to file.....Record " + count + " batch 5");
+
                         }
                     }
                     catch (Exception ex)
